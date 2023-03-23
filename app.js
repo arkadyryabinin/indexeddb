e3c9ce3dc580e198ff2d085a07e689fb91fe92e5
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2023-03-23 15:32:56 +0300

    tests

--- app.js
+++ app.js
@@ -9,22 +9,35 @@
     console.log("This browser doesn't support IndexedDB");
     return;
   }
+
   const dbName = "my-test-database-5";
 
-  const request = indexedDB.open(dbName, 5);
+  const request = indexedDB.open(dbName, 8);
+  console.log(request);
 
   request.onerror = (event) => {
-    // Handle errors.
+    console.log("Error", event.target.error);
   };
+
+  request.onsuccess = (event) => {
+    const db = event.target.result;
+    db.onversionchange = function() {
+      db.close();
+      document.location.reload();
+      console.log("Database is outdated, please reload the page.");
+    };
+  }
+
+
   request.onupgradeneeded = (event) => {
     const db = event.target.result;
-    console.log(db);
+    console.log(event);
 
     // Create an objectStore to hold information about our customers. We're
     // going to use "ssn" as our key path because it's guaranteed to be
     // unique - or at least that's what I was told during the kickoff meeting.
-    // db.close();
-    // db.deleteObjectStore('customers');
+
+    if (event.oldVersion !== 0) db.deleteObjectStore('customers');
     const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
 
     // Create an index to search customers by name. We may have duplicates
